9d945df199fad2c00cf6f8cbe61b923e8692add3
cbw <1625213507@qq.com>
2022-11-15 22:02:41 +0800

    third commit

--- src/main/java/com/atguigu/idea_git.java
+++ src/main/java/com/atguigu/idea_git.java
@@ -4,5 +4,6 @@ public class idea_git {
     public static void main(String[] args) {
         System.out.println("idea_git");
         System.out.println("idea_git222222");
+        System.out.println("idea_git333333333");
     }
 }
