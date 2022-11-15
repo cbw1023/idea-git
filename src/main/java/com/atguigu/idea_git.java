7cd1f804c6f5e5b72c9e9ef314fdd5719c5e89bd
cbw <1625213507@qq.com>
2022-11-15 22:01:55 +0800

    second commit

--- src/main/java/com/atguigu/idea_git.java
+++ src/main/java/com/atguigu/idea_git.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class idea_git {
     public static void main(String[] args) {
         System.out.println("idea_git");
+        System.out.println("idea_git222222");
     }
 }
